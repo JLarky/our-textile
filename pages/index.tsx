74312fbb387623dfc4da9fa9dabd2cef0331c6f3
JLarky <jlarky@gmail.com>
2020-06-15 09:46:33 -0700

    update text

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,13 +23,28 @@ export const Home: React.FC = () => {
         <p className="p-2">
           По всем интересующим вопросам
           <br />
-          +79101999159 Светлана (WhatsApp/Telegram)
+          +79101999159 Светлана (
+          <a href="https://wa.me/79101999159">WhatsApp</a>/
+          <a href="tg://resolve?domain=lapinasm">Telegram</a>)
         </p>
 
         <div className="flex justify-between pt-6 px-2">
           <div>ИП Лапина С.М.</div>
           <div>с. Борок</div>
         </div>
+      </section>
+      <section className="p-4">
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://photos.app.goo.gl/RVErAzbzbtZYiPKb9"
+          className="block rounded overflow-hidden shadow-lg bg-white cursor-pointer"
+        >
+          <img className="w-full" src="./images/example3.jpg" alt="комплекты" />
+          <div className="px-6 py-4 text-left">
+            <div className="font-bold text-xl">Посмотреть фотографии</div>
+          </div>
+        </a>
       </section>
     </Layout>
   )
